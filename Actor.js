0aee38da68a4027af77fa20b64402e1e0ad6438e
Elijah Newman-Gomez <contact@smsithlord.com>
2015-05-13 01:56:37 -0700

    Shooting, enemies, and rotation.

--- Actor.js
+++ Actor.js
@@ -1,10 +1,16 @@
 function Actor(params)
 {
+	this.spawnX = params.x;
+	this.spawnY = params.y;
+	this.spawnZ = params.z;
+	this.spawnRotation = params.rotation;
 	this.gameBoard = params.gameBoard;
 	this.modelName = params.modelName;
+	this.aiClassName = params.aiClassName;
 
-	this.ai = eval("new " + params.aiClassName + "(this);");
-
+	this.ai = null;
+	this.team = null;
+	this.collideRadius = null;
 	this.gameEvent = null;
 	this.sceneObject = null;
 
@@ -18,6 +24,11 @@ Actor.prototype.onTick = function()
 
 	this.ai.onTick();
 }
+
+Actor.prototype.destroy = function()
+{
+	this.gameBoard.scene.remove(this.sceneObject);
+};
 
 Actor.prototype.deltaRotate = function(degrees, axis)
 {
@@ -35,13 +46,26 @@ Actor.prototype.deltaRotate = function(degrees, axis)
     this.sceneObject.rotation.setFromRotationMatrix(this.sceneObject.matrix);
 };
 
+Actor.prototype.orbitWorldOrigin = function(degrees)
+{
+    var rotWorldMatrix = new THREE.Matrix4();
+    rotWorldMatrix.makeRotationAxis(new THREE.Vector3(1,0,0).normalize(), (Math.PI / 180) * degrees);
+    rotWorldMatrix.multiply(this.sceneObject.matrix);
+    this.sceneObject.matrix = rotWorldMatrix;
+    this.sceneObject.rotation.setFromRotationMatrix(this.sceneObject.matrix);
+};
+
 Actor.prototype.init = function()
 {
+	this.ai = eval("new " + this.aiClassName + "(this);");
+
 	// loader assumes .mtl file has same basename as .obj file
 	var filename = this.modelName;
 
 	var thisActor = this;
 	this.gameBoard.loader.load(filename, function ( loadedObject ) {
+//		thisActor.ai = eval("new " + thisActor.aiClassName + "(thisActor);");
+
 		thisActor.sceneObject = loadedObject;
 
 		loadedObject.position.x = 0;
@@ -49,6 +73,34 @@ Actor.prototype.init = function()
 		loadedObject.position.z = 0;
 
 		thisActor.deltaRotate(90, new THREE.Vector3(0, 1, 0));
+		thisActor.deltaRotate(90, new THREE.Vector3(1, 0, 0));
+
+//			thisActor.deltaRotate(45, new THREE.Vector3(1, 0, 0));
+
+			// Orbit the whole thing so it looks like it's coming out of the wall
+		if( thisActor.gameBoard.rotationEnabled )
+			thisActor.orbitWorldOrigin(thisActor.gameBoard.rotation);
+
+		if( typeof thisActor.spawnRotation != 'undefined' )
+		{
+			loadedObject.rotation.x = thisActor.spawnRotation.x;
+			loadedObject.rotation.y = thisActor.spawnRotation.y;
+			loadedObject.rotation.z = thisActor.spawnRotation.z;
+
+			//thisActor.deltaRotate(thisActor.spawnRotation.y, new THREE.Vector3(0, 0, 1));
+		}
+
+/*
+		if( typeof thisActor.lookAt != 'undefined' )
+		{
+			loadedObject.lookAt(thisActor.lookAt);
+		}
+*/
+
+		loadedObject.translateX(thisActor.spawnX);
+		loadedObject.translateY(-thisActor.spawnY);
+		loadedObject.translateZ(thisActor.spawnZ);
+
 //		thisActor.deltaRotate(5);
 
 		var scale = new THREE.Vector3( thisActor.gameBoard.scaleFactor, thisActor.gameBoard.scaleFactor, thisActor.gameBoard.scaleFactor);
@@ -57,31 +109,368 @@ Actor.prototype.init = function()
 			loadedObject.scale.copy( scale );
 
 		thisActor.gameBoard.scene.add(loadedObject);
-		thisActor.gameBoard.cursorEvents.addObject(loadedObject);
+//		thisActor.gameBoard.cursorEvents.addObject(loadedObject);
 	});
 };
 
-function EnemyBee(actor)
+Actor.prototype.setGameEvent = function(params)
 {
+	if( !this.gameEvent || this.gameEvent.priority < params.priority )
+		this.gameEvent = params;
+};
+
+function EnemyShip(actor)
+{
+	this.gameBoard = actor.gameBoard;
+
 	this.actor = actor;
-	this.sequenceName = null;
+	this.actor.team = 1;
+	this.gameBoard.lastSpawnedEnemy = this.gameBoard.tickCount;
+	this.actor.collideRadius = 7.0;
+
+	this.health = 100;
+	this.damageAmount = 100;
+
+	this.sequence = null;
 	this.sequences = {};
 
-	var sequence = {};
-	sequence.name = "straight-down";
-//	sequence.absoluteRotation = 
-	sequence.deltaRotation = null;
+	var sequenceEntrance0 = {};
+	sequenceEntrance0.name = "entrance0";
+	sequenceEntrance0.step = null;
+	sequenceEntrance0.steps = new Array();
+
+	var step0 = {};
+	step0.length = 100;
+	step0.deltaRotate = 0;
+	step0.deltaTranslate = new THREE.Vector3(0, 0, 1);
+	sequenceEntrance0.steps.push(step0);
+
+	var step1 = {};
+	step1.length = 150;
+	step1.deltaRotate = 2;
+	step1.deltaTranslate = new THREE.Vector3(0, 0, 1);
+	sequenceEntrance0.steps.push(step1);
+
+	var step3 = {};
+	step3.length = 100;
+	step3.deltaRotate = 0;
+	step3.deltaTranslate = new THREE.Vector3(0, 0, 1);
+	sequenceEntrance0.steps.push(step3);
+
+	var step4 = {};
+	step4.length = 100;
+	step4.deltaRotate = -2;
+	step4.deltaTranslate = new THREE.Vector3(0, 0, 1);
+	sequenceEntrance0.steps.push(step4);
+
+	var step5 = {};
+	step5.length = 100;
+	step5.deltaRotate = 0;
+	step5.deltaTranslate = new THREE.Vector3(0, 0, 1);
+	sequenceEntrance0.steps.push(step5);
+
+	var step6 = {};
+	step6.length = 4000;
+	step6.deltaRotate = 2;
+	step6.deltaTranslate = new THREE.Vector3(0, 0, 0);
+	sequenceEntrance0.steps.push(step6);
+
+	sequenceEntrance0.animStart = null;
+
+	var thisShip = this;
+	sequenceEntrance0.onTick = function() {
+		var currentStep = this.steps[this.step];
+		if( this.animStart == -1 )
+			this.animStart = this.gameBoard.tickCount;
+
+		if( this.gameBoard.tickCount - this.animStart <= currentStep.length )
+		{
+			this.actor.deltaRotate(currentStep.deltaRotate);
+			this.actor.sceneObject.translateZ(currentStep.deltaTranslate.z);				
+		}
+		else
+		{
+			if( this.step+1 < this.steps.length )
+			{
+				//thisShip.sequence.animStart = this.gameBoard.tickCount;
+				thisShip.sequence.animStart = -1;
+				this.step++;
+
+				thisShip.sequence.onTick();
+			}
+			else
+			{
+				console.log("Path has ended!");
+				this.step = 0;
+				thisShip.sequence = null;
+			}
+		}
+	};
+
+	sequenceEntrance0.actor = this.actor;
+	sequenceEntrance0.gameBoard = this.gameBoard;
+	this.sequences[sequenceEntrance0.name] = sequenceEntrance0;
 }
 
-EnemyBee.prototype.onTick = function()
+EnemyShip.prototype.playSequence = function(sequence_name, sequence_step)
+{
+	var sequence = this.sequences[sequence_name];
+	if( sequence )
+	{
+		if( typeof sequence_step == 'undefined' )
+			sequence.step = 0;
+		else
+			sequence.step = sequence_step;
+
+		sequence.animStart = -1;
+		this.sequence = sequence;
+	}
+};
+
+EnemyShip.prototype.onTick = function()
 {
 	if( !this.actor.gameEvent )
 	{
-		this.actor.deltaRotate(2);
-		this.actor.sceneObject.translateZ(1);
+		if( this.sequence && this.sequence.onTick )
+		{
+			this.sequence.onTick();
+		}
 	}
 	else
 	{
+		var shouldClearGameEvent = true;
+		if( this.actor.gameEvent )
+		{
+			if( this.actor.gameEvent.eventName == "setLook" )
+			{
+				var oldPitch = this.sceneObject.rotation.x;
+				var oldRoll = this.sceneObject.rotation.z;
+
+				this.actor.sceneObject.lookAt(this.gameBoard.playerCursorPosition);
+				this.actor.sceneObject.rotation.x = oldPitch;
+				this.actor.sceneObject.rotation.y = -this.actor.sceneObject.rotation.y;
+				this.actor.sceneObject.rotation.z = oldRoll;
+			}
+			else if( this.actor.gameEvent.eventName == "damage" )
+			{
+				this.health -= this.actor.gameEvent.amount;
+
+				if( this.health <= 0 )
+				{
+					shouldClearGameEvent = false;
+					this.actor.setGameEvent({eventName: "destroy", priority: 100, stopsSequence: true});
+				}
+				else
+				{
+					if( this.gameBoard.isInAltspace )
+					{
+						var object = this.actor.sceneObject;
+
+						if( !object.userData.origTintColor )
+							object.userData.origTintColor = object.userData.tintColor;
+
+						var orange = new THREE.Color(1, 1, 0);
+						var tintColor = { r: orange.r, g: orange.g, b: orange.b };
+						object.userData.tintColor = tintColor;
+//							object.position.x += 0.001; // hack to force AltRender to redraw scene
+					}
+					else
+					{
+						var orange = new THREE.Color(1, 1, 0);
+
+						var object = this.actor.sceneObject;
+						if( object.material )
+						object.material.ambient = orange;
+					}
+				}
+			}
+			else if( this.actor.gameEvent.eventName == "destroy" )
+			{
+				this.gameBoard.removeActor(this.actor);
+			}
+		}
+
+		if( !this.actor.gameEvent.stopsSequence && this.sequence && this.sequence.onTick )
+		{
+			this.sequence.onTick();
+		}
+
+		// Do work
+		if( shouldClearGameEvent )
+			this.actor.gameEvent = null;
+	}
+};
+
+function PlayerTurret(actor)
+{
+	this.actor = actor;
+	this.actor.team = 0;
+	this.collideRadius = 20.0;
+
+	//this.sceneObject = actor.sceneObject;	// doesn't work for some reason
+	this.gameBoard = actor.gameBoard;
+	this.sequence = null;
+	this.sequences = {};
+
+	var sequenceEntrance0 = {};
+	sequenceEntrance0.name = "entrance0";
+	sequenceEntrance0.step = null;
+	sequenceEntrance0.steps = new Array();
+
+	var step0 = {};
+	step0.length = 10;
+	step0.deltaRotate = 0;
+	step0.deltaTranslate = new THREE.Vector3(0, 0, 1);
+	sequenceEntrance0.steps.push(step0);
+
+	var step1 = {};
+	step1.length = 200;
+	step1.deltaRotate = 1;
+	step1.deltaTranslate = new THREE.Vector3(0, 0, 1);
+	sequenceEntrance0.steps.push(step1);
+
+	sequenceEntrance0.animStart = null;
+
+	var thisShip = this;
+	sequenceEntrance0.onTick = function() {
+		var currentStep = this.steps[this.step];
+
+		if( this.gameBoard.tickCount - this.animStart <= currentStep.length )
+		{
+			this.actor.deltaRotate(currentStep.deltaRotate);
+			this.actor.sceneObject.translateZ(currentStep.deltaTranslate.z);				
+		}
+		else
+		{
+			if( this.step+1 < this.steps.length )
+			{
+				//thisShip.sequence.animStart = this.gameBoard.tickCount;
+				thisShip.sequence.animStart = -1;
+				this.step++;
+
+				thisShip.sequence.onTick();
+			}
+			else
+			{
+				console.log("Path has ended!");
+				this.step = 0;
+				thisShip.sequence = null;
+			}
+		}
+	};
+
+	sequenceEntrance0.actor = this.actor;
+	sequenceEntrance0.gameBoard = this.gameBoard;
+	this.sequences[sequenceEntrance0.name] = sequenceEntrance0;
+}
+
+PlayerTurret.prototype.onTick = function()
+{
+	if( !this.actor.gameEvent )
+	{
+		if( this.sequence && this.sequence.onTick )
+		{
+			this.sequence.onTick();
+		}
+	}
+	else
+	{
+		if( this.actor.gameEvent && this.actor.sceneObject )
+		{
+			if( this.actor.gameEvent.eventName == "setLook" )
+			{
+				var oldPitch = this.actor.sceneObject.rotation.x;
+				var oldRoll = this.actor.sceneObject.rotation.z;
+
+				this.actor.sceneObject.lookAt(this.gameBoard.playerCursorPosition);
+				this.actor.sceneObject.rotation.x = oldPitch;
+				this.actor.sceneObject.rotation.y = -this.actor.sceneObject.rotation.y;
+				this.actor.sceneObject.rotation.z = oldRoll;
+			}
+		}
+
+		if( !this.actor.gameEvent.stopsSequence && this.sequence && this.sequence.onTick )
+		{
+			this.sequence.onTick();
+		}
+
+		// Do work
 		this.actor.gameEvent = null;
 	}
+};
+
+function PlayerLaser(actor)
+{
+	this.actor = actor;
+	this.actor.team = 0;
+	this.actor.collideRadius = 1.0;
+
+	this.health = 1;
+	this.damageAmount = 5000;
+	this.maxDist = 200;
+
+	this.gameBoard = actor.gameBoard;
+}
+
+PlayerLaser.prototype.onTick = function()
+{
+	if( !this.actor.gameEvent )
+	{
+		this.actor.sceneObject.translateZ(-2);
+
+		var i;
+		var count = 0;
+		for( i = 0; i < this.gameBoard.actors.length; i++ )
+		{
+			var actor = this.gameBoard.actors[i];
+			if(actor == this.actor || actor.team == this.actor.team || !actor.sceneObject )
+				continue;
+
+			count++;
+			// Check for collisions.  Note that this is expensive.  It would make more sense for actors that want to detect collisions register themselves with the game board, and it check if any registered objects collide and set game events on them accordingly.
+			var dist = this.actor.sceneObject.position.distanceTo(actor.sceneObject.position);
+
+			if( dist < this.actor.collideRadius + actor.collideRadius )
+			{
+				actor.setGameEvent({eventName: "damage", amount: this.damageAmount, priority: 1, stopsSequence: false});
+				this.actor.setGameEvent({eventName: "damage", amount: actor.ai.damageAmount, priority: 1, stopsSequence: false});
+			}
+		}
+	}
+	else
+	{
+		var shouldClearGameEvent = true;
+		if( this.actor.gameEvent && this.actor.sceneObject )
+		{
+			if( this.actor.gameEvent.eventName == "damage" )
+			{
+				this.health -= this.actor.gameEvent.amount;
+
+				if( this.health <= 0 )
+				{
+					shouldClearGameEvent = false;
+					this.actor.setGameEvent({eventName: "destroy", priority: 100, stopsSequence: true});
+				}
+			}
+			else if( this.actor.gameEvent.eventName == "destroy" )
+			{
+				this.gameBoard.removeActor(this.actor);
+				//this.actor.destroy();
+			}
+		}
+
+		if( !this.actor.gameEvent.stopsSequence && this.sequence && this.sequence.onTick )
+		{
+			this.sequence.onTick();
+		}
+
+		// Do work
+		if( shouldClearGameEvent )
+			this.actor.gameEvent = null;
+	}
+
+	var lifeDist = this.actor.sceneObject.position.distanceTo(this.actor.gameBoard.playerTurret.sceneObject.position);
+
+	if( lifeDist > this.maxDist && this.actor.gameEventName != "destroy" )
+		this.actor.setGameEvent({eventName: "destroy", priority: 100, stopsSequence: true});
 };
--- GameBoard.js
+++ GameBoard.js
@@ -1,15 +1,41 @@
 function GameBoard()
 {
 	this.isInAltspace = null;
+	this.rotationEnabled = true;
+	this.rotation = -70;	//&rotation
 
+	var loc = document.location.href;
+	var foundIndex = loc.indexOf("?rotation=");
+	if( foundIndex != -1 )
+	{
+		var rot = eval(loc.substring(foundIndex+1)) * -1.0;
+		this.rotation = rot;
+
+		if( this.rotation == 0 )
+			this.rotationEnabled = false;
+	}
+
+	this.tickCount = 0;
 	this.renderer = null;
 	this.scene = null;
+	this.raycaster = null;
 	this.cursorEvents = null;
+	this.playerCursorPosition = null;
+	this.dragPlane = null;
+	this.listener = null;
 	this.loader = null;
 	this.actors = null;
+	this.scaleFactor = 1.0;
+	this.playerTurret = null;
+	this.lastSpawnedEnemy = 0;
+
+	if(!!window.Alt)
+		this.scaleFactor = 5.0;
+
+	this.yOffset = 60 * this.scaleFactor;
 
 	// Only used while INSIDE of AltspaceVR
-	this.scaleFactor = 5.0;
+
 
 	// Only used while OUTSIDE of AltspaceVR
 	this.camera = null;
@@ -18,9 +44,47 @@ function GameBoard()
 	// Initialize everything
 	this.init();
 
+	// Precache some stuff
+	this.loader.load("models/InterD/cube.obj", function ( loadedObject ) {
+//		thisActor.gameBoard.scene.add(loadedObject);
+	});
+
+	this.loader.load("models/InterD/player_turret.obj", function ( loadedObject ) {
+//		thisActor.gameBoard.scene.add(loadedObject);
+	});
+
+	this.loader.load("models/InterD/player_laser.obj", function ( loadedObject ) {
+//		thisActor.gameBoard.scene.add(loadedObject);
+	});
+
 	// Start the simulation
 	this.tick();
 }
+
+GameBoard.prototype.deltaDragPlaneRotate = function(degrees, axis)
+{
+	var rotationAxis;
+
+	if( typeof axis != 'undefined' )
+		rotationAxis = axis;
+	else
+		rotationAxis = new THREE.Vector3(-1,0,0);
+
+   	var rotObjectMatrix = new THREE.Matrix4();
+   	rotObjectMatrix.makeRotationAxis(rotationAxis.normalize(), (Math.PI / 180) * degrees);
+
+	this.dragPlane.matrix.multiply(rotObjectMatrix);
+    this.dragPlane.rotation.setFromRotationMatrix(this.dragPlane.matrix);
+};
+
+GameBoard.prototype.orbitDragPlaneWorldOrigin = function(degrees)
+{
+    var rotWorldMatrix = new THREE.Matrix4();
+    rotWorldMatrix.makeRotationAxis(new THREE.Vector3(1,0,0).normalize(), (Math.PI / 180) * degrees);
+    rotWorldMatrix.multiply(this.dragPlane.matrix);
+    this.dragPlane.matrix = rotWorldMatrix;
+    this.dragPlane.rotation.setFromRotationMatrix(this.dragPlane.matrix);
+};
 
 GameBoard.prototype.init = function()
 {
@@ -30,6 +94,7 @@ GameBoard.prototype.init = function()
 
 	// Initialize the scene
 	this.scene = new THREE.Scene();
+	this.raycaster = new THREE.Raycaster();
 
 	if( this.isInAltspace )
 	{
@@ -45,25 +110,107 @@ GameBoard.prototype.init = function()
 
 		var aspect = window.innerWidth / window.innerHeight;
 		this.camera = new THREE.PerspectiveCamera(45, aspect, 1, 2000 );
-		this.camera.position.z = 300;
+		this.camera.position.z = 70;
 		this.camera.position.y = 0;
 		this.camera.lookAt( this.scene.position );
+		this.camera.position.y = 20;
+
+		if( !this.rotationEnabled )
+		{
+			this.camera.translateY(80);
+			this.camera.translateZ(230);	// 230
+		}
+
+		this.listener = document.createElement("div");
+		this.listener.style.display = "none";
+		this.listener.addEventListener( "holocursormove", this.onCursorMove);
+		this.listener.board = this;
 
 		this.ambient = new THREE.AmbientLight( 0xffffff );
 		this.scene.add( this.ambient );
 	}
 
 	// Initialize the cursor events
-	this.cursorEvents = new CursorEvents();
+	//this.cursorEvents = new CursorEvents();
+	var evenParams = { defaultTarget: this.listener };
+	this.cursorEvents = new CursorEvents(evenParams);
+
 	this.cursorEvents.enableMouseEvents(this.camera);	// It's OK to pass a null pointer here if we're inside of AltspaceVR
+
+	// Add the drag plane and apply the same rotation to it as actors do.
+	this.dragPlane = new THREE.Mesh( 
+      new THREE.BoxGeometry(120 * 1.5, 100 * 1.5, 0.25),
+      new THREE.MeshBasicMaterial( { color: "#ff0000", transparent: true, opacity: 0.25 })
+    );
+
+    this.dragPlane.position.x = 0;
+    this.dragPlane.position.y = 0;
+    this.dragPlane.position.z = 0;
+
+	this.dragPlane.up = new THREE.Vector3(0,0,1);
+
+   	this.deltaDragPlaneRotate(90, new THREE.Vector3(0, 1, 0));
+	this.deltaDragPlaneRotate(-90, new THREE.Vector3(1, 0, 0));
+//			thisActor.deltaRotate(45, new THREE.Vector3(1, 0, 0));
+
+	// Orbit the whole thing so it looks like it's coming out of the wall
+	this.orbitDragPlaneWorldOrigin(90);
+
+    if( this.rotationEnabled )
+		this.orbitDragPlaneWorldOrigin(this.rotation);
+	//else
+//		this.orbitDragPlaneWorldOrigin(90);
+
+	this.dragPlane.translateZ(0);
+	this.dragPlane.translateX(110);
+
+    this.scene.add(this.dragPlane);
+};
+
+GameBoard.prototype.rayCast = function(ray)
+{
+	if( typeof this.playerTurret.sceneObject == 'undefined' )
+		return;
+
+	this.raycaster.set( ray.origin, ray.direction );
+
+	var intersects = this.raycaster.intersectObjects( [this.dragPlane] );
+
+	if ( intersects.length > 0 )
+	{
+		this.playerCursorPosition = intersects[0].point;
+
+		// Instead of modifying game objects here, just set a game event that the board handles during the next tick.
+		this.playerTurret.setGameEvent({eventName: "setLook", priority: 0, stopsSequence: false});
+	}
+};
+
+// Note that the "this" pointer in this callback will be to the listener object, NOT the GameBoard object itself.
+GameBoard.prototype.onCursorMove = function(e)
+{
+	var board = this.board;
+	board.rayCast(e.detail.cursorRay);
 };
 
 GameBoard.prototype.tick = function()
 {
+	this.tickCount++;
+
+	var numEnemies = 0;
 	var i;
 	for( i = 0; i < this.actors.length; i++)
 	{
+		if( this.playerTurret && this.playerTurret.team != this.actors[i].team )
+			numEnemies++;
+
 		this.actors[i].onTick();
+	}
+
+	if( numEnemies < 5 && this.tickCount - this.lastSpawnedEnemy > 200)
+	{
+		this.spawnActor({aiClassName: "EnemyShip", modelName: "models/InterD/cube.obj", x: 0, y: Math.random() * 80, z: -200}).ai.playSequence("entrance0");
+		this.spawnActor({aiClassName: "EnemyShip", modelName: "models/InterD/cube.obj", x: 0, y: Math.random() * 80, z: -200}).ai.playSequence("entrance0");
+		this.spawnActor({aiClassName: "EnemyShip", modelName: "models/InterD/cube.obj", x: 0, y: Math.random() * 80, z: -200}).ai.playSequence("entrance0");
 	}
 
 	var thisGameBoard = this;
@@ -82,4 +229,100 @@ GameBoard.prototype.spawnActor = function(params)
 
 	var actor = new Actor(alteredParams);
 	this.actors.push(actor);
+
+	return actor;
+};
+
+GameBoard.prototype.removeActor = function(actor)
+{
+	var i;
+	for( i = 0; i < this.actors.length; i++ )
+	{
+		if( this.actors[i] == actor )
+		{
+			this.actors.splice(i, 1);
+		}
+	}
+
+	actor.destroy();
+};
+
+GameBoard.prototype.reset = function(rotEnabled)
+{
+	var i;
+	for( i = 0; i < this.actors.length; i++ )
+	{
+		this.actors.splice(i, 1);
+		this.scene.remove(this.actors[i].sceneObject);
+	}
+
+	this.rotationEnabled = rotEnabled;
+
+	this.tickCount = 0;
+	this.playerCursorPosition = null;
+	this.actors = null;
+	this.playerTurret = null;
+	this.lastSpawnedEnemy = 0;
+
+	// Initialize everything
+	this.reInit();	
+};
+
+GameBoard.prototype.reInit = function()
+{
+	this.actors = new Array();
+
+	// Reset the dragplane
+	this.dragPlane.rotation.x = 0;
+	this.dragPlane.rotation.y = 0;
+	this.dragPlane.rotation.z = 0;
+
+	this.dragPlane.position.x = 0;
+	this.dragPlane.position.y = 0;
+	this.dragPlane.position.z = 0;
+
+	this.dragPlane.up = new THREE.Vector3(0,0,1);
+
+// 	this.deltaDragPlaneRotate(90, new THREE.Vector3(0, 1, 0));
+//	this.deltaDragPlaneRotate(-90, new THREE.Vector3(1, 0, 0));
+
+	// Orbit the whole thing so it looks like it's coming out of the wall
+    if( this.rotationEnabled )
+		this.orbitDragPlaneWorldOrigin(20);
+	else
+		this.orbitDragPlaneWorldOrigin(90);
+	/*
+
+	this.dragPlane.translateZ(0);
+	this.dragPlane.translateX(110);
+	*/
+
+	// Reset the camera
+	if( !this.isInAltspace )
+	{
+			this.camera.rotation.x = 0;
+		this.camera.rotation.y = 0;
+		this.camera.rotation.z = 0;
+
+		this.camera.position.x = 0;
+		this.camera.position.y = 0;
+		this.camera.position.z = 0;
+
+		this.camera.position.z = 70;
+		this.camera.lookAt( this.scene.position );
+		this.camera.position.y = 20;
+
+		if( !this.rotationEnabled )
+		{
+			this.camera.translateY(80);
+			this.camera.translateZ(230);	// 230
+		}
+	}
+};
+
+GameBoard.prototype.playerFire = function()
+{
+	var laserDirection = new THREE.Vector3(this.playerTurret.sceneObject.rotation.x, this.playerTurret.sceneObject.rotation.y, 0);	// Probably need to do more than just 1 rotation and apply it post-spawn.
+	var laser1 = this.spawnActor({aiClassName: "PlayerLaser", modelName: "models/InterD/player_laser.obj", x: 8, y: 0, z: -20, rotation: laserDirection});
+	var laser2 = this.spawnActor({aiClassName: "PlayerLaser", modelName: "models/InterD/player_laser.obj", x: -8, y: 0, z: -20, rotation: laserDirection});
 };
